e17fdcac62067b8db4da875c1a3a6b868d5cd885
William Vincent <compelledtograce@gmail.com>
2020-10-13 18:30:39 -0400

    Sort of, almost done, dude. Bro.

--- index.js
+++ index.js
@@ -2,19 +2,44 @@ const fetch = require("node-fetch");
 const fs = require("fs").promises;
 
 const fetchLuke = fetch("https://swapi.dev/api/people/1");
-
-fetchLuke
-  .then((response) => response.json())
-  .then((person) => console.log(person))
-  .catch((err) => console.error(err));
-
+const fetchFilms = fetch("https://swapi.dev/api/people/1");
 const fetchLukeHome = fetch("http://swapi.dev/api/planets/1/");
 
-fetchLukeHome
-  .then((response) => response.json())
-  .then((person) => console.log(person))
+const fetchedLuke = fetchLuke
+  .then((lukeResponse) => lukeResponse.json())
+  .then((lukeObj) => lukeObj.name)
   .catch((err) => console.error(err));
 
-fs.writeFile("fileName.txt", contents, "utf-8")
-  .then(() => console.log("File has been written sucessfully!"))
-  .catch(() => console.log("File was not written"));
+const fetchedHome = fetchLukeHome
+  .then((homeResponse) => homeResponse.json())
+  .then((homeObj) => homeObj.name)
+  .catch((err) => console.error(err));
+
+const fetchedFilmNames = fetchFilms
+  .then((startResponse) => startResponse.json())
+  .then((startObj) => startObj.films)
+  .then((films) => {
+    let filmNames = [];
+    for (let i = 0; i < films.length; i++) {
+      let film = fetch(films[i]);
+      filmNames.push(film.name);
+    }
+  })
+  .catch((err) => console.error(err));
+
+const getInfo = Promise.all([fetchedLuke, fetchedHome, fetchedFilmNames])
+  .then(() =>
+    console.log(
+      `My name is ${fetchedLuke},
+      I am from ${fetchedHome},
+      and I starred in the following films:
+      ${fetchedFilmNames[0]}, ${fetchedFilmNames[1]},
+      ${fetchedFilmNames[2]}, ${fetchedFilmNames[3]}`
+    )
+  )
+  .catch((err) => console.error(err));
+
+/* My name is Luke Skywalker, I am from Tatooine
+I starred in the following films: A New Hope,
+The Empire Strikes Back, Return of the Jedi,
+Revenge of the Sith */
